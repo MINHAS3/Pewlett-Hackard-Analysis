9dd9d8d8c125713797c7a11da210d382b40457e1
MINHAS3 <66636514+MINHAS3@users.noreply.github.com>
2020-09-18 20:20:29 -0500

    Delete recent_title.sql

--- Queries/recent_title.sql
+++
@@ -1,8 +0,0 @@
-select * from titles order by emp_no,from_date;
-
-
-select t.emp_no, t.title, table1.maxdate into recent_title from titles as t inner join
-(select emp_no,max(from_date) as maxdate from titles group by emp_no)  as table1
-on t.emp_no = table1.emp_no and t.from_Date = table1.maxdate;
-
-select * from recent_title; 
